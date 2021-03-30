36520b5c35f0d3e2c2655c6b862c4fd3abfefc28
gjgd <gjgd@protonmail.com>
2021-03-30 02:13:42 +0000

    feat: add docker-compose setup

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const config = {
   contentAddressableStoreServiceUri: '/ip4/127.0.0.1/tcp/5001',
   databaseName: '',
   didMethodName: '',
-  ethereumRpcUrl: 'http://localhost:8545',
-  mongoDbConnectionString: 'mongodb://localhost:27017',
+  ethereumRpcUrl: 'http://ganache:8545',
+  mongoDbConnectionString: 'mongodb://mongo:27017',
   batchingIntervalInSeconds: 5,
   observingIntervalInSeconds: 5,
   maxConcurrentDownloads: 20,
@@ -20,6 +20,8 @@ const config = {
 };
 
 (async () => {
+  // Wait a few seconds to allow for all the services to initialize
+  await new Promise((resolve) => setTimeout(resolve, 5 * 1000));
   const element = await DidElement.initialize(config);
-  console.log(element);
+  console.log('Element initialized', Boolean(element));
 })();
